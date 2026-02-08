3f449166f2c25e49a84d230f88826519be2e39c3
Torolgo <ylan.dessenne.spam@gmail.com>
2026-02-08 19:48:26 +0100

    feat(strategy): added demo of dynamic delivery change

--- behavioral/strategy/src/Main.java
+++ behavioral/strategy/src/Main.java
@@ -3,9 +3,18 @@ void main() {
     Commande commande2 = new Commande("Loic", 100.0, 20.0, new LivraisonExpress());
     Commande commande3 = new Commande("Romain", 30.0, 5.0, new ClickAndCollect());
 
+    System.out.println("--- Commandes initiales ---");
     commande1.afficherDetails();
     System.out.println();
     commande2.afficherDetails();
     System.out.println();
     commande3.afficherDetails();
+
+    System.out.println("\n--- Changement de stratégie (Démonstration du pattern) ---");
+    System.out.println("Le client Loic trouve la livraison Express trop chère...");
+
+    commande2.setStrategieLivraison(new LivraisonStandard());
+
+    System.out.println("Nouveau calcul pour Loic (Livraison Standard) :");
+    commande2.afficherDetails();
 }
